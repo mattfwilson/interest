99bc4dcef87fce33c59adbc970d811f2f400d51e
mattfwilson <mattfwilson@gmail.com>
2022-06-06 02:20:18 -0400

    Trying to swap lists to label the year bars on the chart

--- invest_graph.py
+++ invest_graph.py
@@ -4,12 +4,16 @@ from matplotlib import pylab
 # variables to play with
 principal = 600000
 interest = .065
+percentage = "{:.2%}".format(interest)
 numCompounds = 12
 yearCounter = 0
 growthYears = 10
 contribution = 36000
-testYears = []
 testInterest = []
+
+left = [1, 2, 3, 4, 5] # x-coordinates of left sides of bars
+height = [10, 24, 36, 40, 5] # heights of bars
+year_label = [] # labels for bars
 
 # calculate compound interest
 def calcInterest(principal, interest, numCompounds, contribution):
@@ -25,18 +29,13 @@ while yearCounter < growthYears:
     principal = round(principal, 2)
     interestDelta = round(interestDelta, 2)
     testInterest.append(principal)
-    testYears.append(interestDelta)
+    year_label.append(interestDelta)
     print(f'Year {yearCounter} Total: ${principal} | Interest: ${interestDelta}')
     print(f'{testInterest}')
 
-left = [1, 2, 3, 4, 5] # x-coordinates of left sides of bars
-height = [10, 24, 36, 40, 5] # heights of bars
-tick_label = ['one', 'two', 'three', 'four', 'five'] # labels for bars
-
 # plot title
-percentage = "{:.2%}".format(interest)
 plt.title(f'Compound interest over {growthYears} years at {percentage}')
-plt.bar(left, height, tick_label = tick_label, width = 0.8, color = ['orange', 'green']) # plotting a bar chart
+plt.bar(left, height, year_label = year_label, width = 0.8, color = ['orange', 'green']) # plotting a bar chart
 plt.ylabel('Value in dollars') # naming the y-axis
 plt.xlabel('Years of compounding')# naming the x-axis
 plt.show() # function to show the plot chart
