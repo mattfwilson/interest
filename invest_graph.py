0babb49860f6cd96ebbb5dfcf6912e40e2761913
mattfwilson <mattfwilson@gmail.com>
2022-06-06 01:34:30 -0400

    Added appending of interest and years to test lists

--- invest_graph.py
+++ invest_graph.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from matplotlib import pylab
 
 # variables to play with
 principal = 600000
@@ -7,6 +8,8 @@ numCompounds = 12
 yearCounter = 0
 growthYears = 10
 contribution = 36000
+testYears = []
+testInterest = []
 
 # calculate compound interest
 def calcInterest(principal, interest, numCompounds, contribution):
@@ -17,33 +20,23 @@ def calcInterest(principal, interest, numCompounds, contribution):
 
 # show YOY summary
 while yearCounter < growthYears:
-	yearCounter += 1
-	principal, interestDelta = calcInterest(principal, interest, numCompounds, contribution)
-	principal = round(principal, 2)
-	interestDelta = round(interestDelta, 2)
-	print(f'Year {yearCounter} Total: ${principal} | Interest: ${interestDelta}')
+    yearCounter += 1
+    principal, interestDelta = calcInterest(principal, interest, numCompounds, contribution)
+    principal = round(principal, 2)
+    interestDelta = round(interestDelta, 2)
+    testInterest.append(principal)
+    testYears.append(interestDelta)
+    print(f'Year {yearCounter} Total: ${principal} | Interest: ${interestDelta}')
+    print(f'{testInterest}')
 
-# x-coordinates of left sides of bars
-left = [1, 2, 3, 4, 5]
- 
-# heights of bars
-height = [10, 24, 36, 40, 5]
- 
-# labels for bars
-tick_label = ['one', 'two', 'three', 'four', 'five']
- 
-# plotting a bar chart
-plt.bar(left, height, tick_label = tick_label, width = 0.8, color = ['orange', 'green'])
+left = [1, 2, 3, 4, 5] # x-coordinates of left sides of bars
+height = [10, 24, 36, 40, 5] # heights of bars
+tick_label = ['one', 'two', 'three', 'four', 'five'] # labels for bars
 
 # plot title
 percentage = "{:.2%}".format(interest)
 plt.title(f'Compound interest over {growthYears} years at {percentage}')
-
-# naming the y-axis
-plt.ylabel('Value in dollars')
-
-# naming the x-axis
-plt.xlabel('Years of compounding')
- 
-# function to show the plot chart
-plt.show()
+plt.bar(left, height, tick_label = tick_label, width = 0.8, color = ['orange', 'green']) # plotting a bar chart
+plt.ylabel('Value in dollars') # naming the y-axis
+plt.xlabel('Years of compounding')# naming the x-axis
+plt.show() # function to show the plot chart
