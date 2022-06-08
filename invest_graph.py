bdd296edf5f021882b006e60905a3984f83bbb14
mattfwilson <mattfwilson@gmail.com>
2022-06-07 23:58:59 -0400

    Added a new list to hold the deltas of the yearly interest accrued

--- invest_graph.py
+++ invest_graph.py
@@ -10,6 +10,7 @@ yearCounter = 0
 growthYears = 10
 contribution = 36000
 testInterest = []
+interestDelta = []
 left = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] # x-coordinates of left sides of bars
 height = [10, 24, 36, 40, 5] # heights of bars
 tick_label = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] # labels for bars
@@ -40,5 +41,6 @@ while yearCounter < growthYears:
 # plt.show() # function to show the plot chart
 
 for i in range(1, len(testInterest)):
-    test = testInterest[i] - testInterest[i -1]
-    print(test)
+    delta = testInterest[i] - testInterest[i -1]
+    interestDelta.append(delta)
+    print(interestDelta)
