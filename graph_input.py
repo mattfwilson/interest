bd59dacfc3916f39439fd274604a18c16cbfdff4
mattfwilson <mattfwilson@gmail.com>
2022-06-22 02:00:16 -0400

    Figured out the dollar/comma formatting for money questions

--- graph_input.py
+++ graph_input.py
@@ -1,7 +1,5 @@
 import matplotlib.pyplot as plt
-from matplotlib import pylab
 
-# variables to play with
 principal = 0
 interest = 0
 percentage = "{:.2%}".format(interest)
@@ -23,25 +21,25 @@ def calcInterest(principal, interest, numCompounds, contribution):
 	principal += contribution
 	return principal, accrued
 
-# show YOY summary
-# while yearCounter < growthYears:
-#     yearCounter += 1
-#     principal, interestDelta = calcInterest(principal, interest, numCompounds, contribution)
-#     principal = round(principal, 2)
-#     interestDelta = round(interestDelta, 2)
-#     testInterest.append(principal)
-
+# user inputted vars
 principal = input('What is your base principal amount? ')
-print(f'Okay, your starting principal is ${principal}.')
+principal = float(principal)
+formatted_dollars = "${:,.2f}".format(principal)
+print(f'Okay, your starting principal is {formatted_dollars}.')
+
 interest = input(f'What is your assumed avg % interest rate? ')
 decimal_interest = float(interest) / 100
-print(f'Nice, {interest}%.')
-contribution = input('What is your total annual contribution? ')
-print(f'Excellent, ${contribution} will help your investment grow.')
-growthYears = input('How many years before you retire? ')
-print(f'{growthYears}, you\'ll be there before you know it!')
+print(f'Nice, {interest}% interest.')
 
-# show chart
+contribution = input('What is your total annual contribution? ')
+contribution = float(contribution)
+formatted_contribution = "${:,.2f}".format(contribution)
+print(f'Excellent, contributing {formatted_contribution} a year will help your investments grow.')
+
+growthYears = input('How many years before you retire? ')
+print(f'{growthYears} years of growth, you\'ll be there before you know it!')
+
+# display bar graph of investments over time
 plt.title(f'Compound interest over {growthYears} years at {percentage}')
 plt.bar(left, testInterest, tick_label = tick_label, width = .75, color = ['green']) # plotting a bar chart
 plt.ylabel('Total value (by $Millions)') # naming the y-axis
