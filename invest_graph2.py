e312f3d7a92158657931c77e0d892c6825b818f5
mattfwilson <mattfwilson@gmail.com>
2022-08-22 11:52:43 -0400

    Removes notes and unnecessary parts of the main while loop/global variables.

--- invest_graph2.py
+++ invest_graph2.py
@@ -1,19 +1,17 @@
 import matplotlib.pyplot as plt
 from matplotlib import pylab
 
-# variables to play with
-principal = 50000
-interest = .075
+# variables
+principal = 650000
+interest = .08
 percentage = "{:.2%}".format(interest)
 numCompounds = 1
 yearCounter = 0
-growthYears = 20
-contribution = 36000
-testDollars = []
+growthYears = 8
+contribution = 40000
 testInterest = []
-interestDelta = []
-left = [] # x-coordinates of left sides of bars
-tick_label = [] # labels for bars
+left_label = []
+bottom_label = []
 
 # calculate compound interest
 def calcInterest(principal, interest, numCompounds, contribution):
@@ -22,19 +20,18 @@ def calcInterest(principal, interest, numCompounds, contribution):
 	principal += contribution
 	return principal, accrued
 
-# show YOY summary
+# calculate YOY totals
 while yearCounter < growthYears:
     yearCounter += 1
     principal, interestDelta = calcInterest(principal, interest, numCompounds, contribution)
     principal = round(principal, 2)
-    interestDelta = round(interestDelta, 2)
     testInterest.append(principal)
-    left.append(yearCounter)
-    tick_label.append(yearCounter)
+    left_label.append(yearCounter)
+    bottom_label.append(yearCounter)
 
-# show chart
+# chart YOY totals
 plt.title(f'Compound interest over {growthYears} years at {percentage}')
-plt.bar(left, testInterest, tick_label = tick_label, width = .75, color = ['green']) # plotting a bar chart
-plt.ylabel('Total value (by $Millions)') # naming the y-axis
-plt.xlabel('Years of compounding')# naming the x-axis
-plt.show() # function to show the plot chart
+plt.bar(left_label, testInterest, tick_label = bottom_label, width = .75, color = ['green'])
+plt.ylabel('Total value (by $Millions)')
+plt.xlabel('Years of compounding')
+plt.show()
