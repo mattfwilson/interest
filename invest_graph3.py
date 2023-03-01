64480828b2b86699ff3998f0b592d21cf570a6fd
mattfwilson <mattfwilson@gmail.com>
2023-03-01 03:44:01 -0500

    Renaming of variables

--- invest_graph3.py
+++ invest_graph3.py
@@ -1,7 +1,5 @@
 import matplotlib.pyplot as plt
-from matplotlib import pylab
 
-# variables
 principal = 650000
 int_rate = .05
 percentage = "{:.2%}".format(int_rate)
@@ -14,23 +12,18 @@ testInterest = []
 left_label = []
 bottom_label = []
 
-# calculate compound int_rate
-def calc_compound(principal, int_rate, num_comp, years, contribution):
-	amount = principal * (pow((1 + int_rate / num_comp), num_comp * (years + 1)))
-	principal = principal + amount
-	principal += contribution
-	return principal, amount
+def calc_compound(p, r, n, t):
+    for period in range(t):
+        acrrued = p * (pow((1 + r / n), n * (period + 1)))
+        print('Period:', period + 1, acrrued)
+    return acrrued
 
-# calculate YOY totals
-while yearCounter < growthYears:
-    yearCounter += 1
-    principal, interestDelta = calc_compound(principal, int_rate, num_comp, growthYears, contribution)
-    principal = round(principal, 2)
-    testInterest.append(principal)
-    left_label.append(yearCounter)
-    bottom_label.append(yearCounter)
+principal = calc_compound(principal, int_rate, num_comp, growthYears)
+principal = round(principal, 2)
+testInterest.append(principal)
+left_label.append(yearCounter)
+bottom_label.append(yearCounter)
 
-# chart YOY totals
 plt.title(f'Compound int_rate over {growthYears} years at {percentage} = ${principal}')
 plt.bar(left_label, testInterest, tick_label = bottom_label, width = .75, color = ['green'])
 plt.ylabel('Total value (by $Millions)')
