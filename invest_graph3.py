473e9ee0a3f19027296ab8b7d0a7f5b879003730
mattfwilson <mattfwilson@gmail.com>
2023-03-01 04:44:35 -0500

    Cleaning up syntax and rounding issues

--- invest_graph3.py
+++ invest_graph3.py
@@ -1,13 +1,12 @@
 import matplotlib.pyplot as plt
 
-principal = 650000
+principal = 250000
 int_rate = .05
 percentage = "{:.2%}".format(int_rate)
 num_comp = 12
-yearCounter = 0
 growthYears = 15
-contribution = 40000
-contribution_inc = .02
+annual_contribution = 10000
+
 testInterest = []
 left_label = []
 bottom_label = []
@@ -15,17 +14,18 @@ bottom_label = []
 def calc_compound(p, r, n, t):
     for period in range(t):
         acrrued = p * (pow((1 + r / n), n * (period + 1)))
-        print('Period:', period + 1, acrrued)
+        print(f'Year {period + 1} - ${round(acrrued, 2)}')
     return acrrued
 
 principal = calc_compound(principal, int_rate, num_comp, growthYears)
 principal = round(principal, 2)
 testInterest.append(principal)
-left_label.append(yearCounter)
-bottom_label.append(yearCounter)
+left_label.append(principal)
+bottom_label.append(range(growthYears))
 
+# graph with matplot
 plt.title(f'Compound int_rate over {growthYears} years at {percentage} = ${principal}')
-plt.bar(left_label, testInterest, tick_label = bottom_label, width = .75, color = ['green'])
+plt.bar(left_label, testInterest, tick_label=bottom_label, width=.75, color=['green'])
 plt.ylabel('Total value (by $Millions)')
 plt.xlabel('Years of compounding')
 plt.show()
