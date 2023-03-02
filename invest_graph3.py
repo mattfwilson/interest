7c4e39a4a83253f83f1f9e73c07e2f8b5229a50c
mattfwilson <mattfwilson@gmail.com>
2023-03-01 19:15:28 -0500

    Removed unnecessary vars

--- invest_graph3.py
+++ invest_graph3.py
@@ -1,31 +1,33 @@
-import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('TkAgg',force=True)
+from matplotlib import pyplot as plt
 
-principal = 250000
-int_rate = .05
+principal = 1000
+int_rate = .06
 percentage = "{:.2%}".format(int_rate)
 num_comp = 12
-growthYears = 15
+growthYears = 5
 annual_contribution = 10000
 
-testInterest = []
 left_label = []
 bottom_label = []
 
-def calc_compound(p, r, n, t):
-    for period in range(t):
-        acrrued = p * (pow((1 + r / n), n * (period + 1)))
-        print(f'Year {period + 1} - ${round(acrrued, 2)}')
+def calc_compound(P, r, n, t):
+    for year in range(t):
+        acrrued = P * (pow((1 + r / n), n * (year + 1)))
+        print(f'Year {year + 1} - ${round(acrrued, 2)}')
     return acrrued
 
 principal = calc_compound(principal, int_rate, num_comp, growthYears)
-principal = round(principal, 2)
-testInterest.append(principal)
 left_label.append(principal)
 bottom_label.append(range(growthYears))
 
+print(f'Length of left label: {len(left_label)}')
+print(f'Length of bottom label: {len(bottom_label)}')
+
 # graph with matplot
-plt.title(f'Compound int_rate over {growthYears} years at {percentage} = ${principal}')
-plt.bar(left_label, testInterest, tick_label=bottom_label, width=.75, color=['green'])
-plt.ylabel('Total value (by $Millions)')
-plt.xlabel('Years of compounding')
-plt.show()
+# plt.title(f'Compound int_rate over {growthYears} years at {percentage} = ${principal}')
+# plt.bar(left_label, principal, tick_label=bottom_label, width=.75, color=['green'])
+# plt.ylabel('Total value (by $Millions)')
+# plt.xlabel('Years of compounding')
+# plt.show()
