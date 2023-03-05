4a798bd8f0d6d900ca723c1796752a24292c651e
mattfwilson <mattfwilson@gmail.com>
2023-03-05 18:42:56 -0500

    Took out contribution for now (buggy). Added user input back in that checks for empty int_rate and uses historical average if null

--- invest_graph3.py
+++ invest_graph3.py
@@ -3,21 +3,21 @@ matplotlib.use('TkAgg', force=True)
 from matplotlib import pyplot as plt
 from returns import *
 
-principal = 700000
-int_rate = .0752
+principal = 0
+int_rate = 0
 num_comp = 12
 comp_years = 15
 counter = 0
-contribution = 40000
+contribution = 75000
 left_label = []
 bottom_label = []
 total = []
 int_accrued = []
 
 # compounding func appending each year to total list
-def calc_compound(principal, rate, num_comp, time, contribution):
+def calc_compound(principal, rate, num_comp, time):
     for year in range(time):
-        amount = (principal + contribution) * (pow((1 + rate / num_comp), num_comp * (year + 1)))
+        amount = principal * (pow((1 + rate / num_comp), num_comp * (year + 1)))
         total.append(amount)
         print(f'Year {year + 1} - ${round(amount, 2)}')
     return amount
@@ -30,10 +30,13 @@ def calc_interest(principal, rate, time):
     return amount
 
 # input
-# principal = int(input('How much do you currently have saved? '))
-# comp_years = int(input('How many years until retirement? '))
-# int_rate = float(input('What is the current interest rate? '))
-percentage = '{:.2%}'.format(int_rate)
+principal = int(input('How much do you currently have saved? '))
+comp_years = int(input('How many years until retirement? '))
+int_input = input('What is the current interest rate? ')
+if int_input == '':
+    int_rate = float(hist_interest)
+else:
+    int_rate = float(int_input)
 
 # count increment for graph tick labels
 while counter < comp_years:
@@ -42,16 +45,19 @@ while counter < comp_years:
     bottom_label.append(counter)
 
 # instantiate calc_compound
-compounded = round(calc_compound(principal, int_rate, num_comp, comp_years, contribution), 2)
+compounded = round(calc_compound(principal, int_rate, num_comp, comp_years), 2)
+percentage = '{:.2%}'.format(int_rate)
 dollars = '{:,}'.format(compounded)
 
 # graph data with matplot
 fig, ax = plt.subplots(figsize=(12, 9))
 fig.suptitle(f'Compound principal + accrued interest over {comp_years} years', fontsize='20', fontweight='bold')
 plt.title(f'Interest Rate: {percentage}, Total: ${dollars}', fontsize='12', fontweight='regular')
+
 plt.bar(left_label, total, tick_label=bottom_label, width=.5, color=['green'])
 plt.ylabel('Total value', fontweight='bold')
 ax.xaxis.set_label_coords(.5, -.07)
 plt.xlabel('Years of compounding', fontweight='bold')
 ax.yaxis.set_label_coords(-.1, .5)
+
 plt.show()
