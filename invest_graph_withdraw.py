f34e3189e02eda9e96373f408034fcf5b0676301
mattfwilson <mattfwilson@gmail.com>
2023-03-15 11:20:22 -0400

    Added year labels list and took out import for calculating the S&P average return

--- invest_graph_withdraw.py
+++ invest_graph_withdraw.py
@@ -8,25 +8,25 @@ import matplotlib
 matplotlib.use('TkAgg', force=True)
 from matplotlib import pyplot as plt
 from matplotlib.ticker import MaxNLocator
-from returns import *
 
 principal = 650000
 int_rate = .075
 num_comp = 12
-comp_years = 15
+comp_years = 12
 counter = 0
 contribution = 22500
 withdrawal = 75000
 x_labels = []
 y_labels = []
-year_total = []
+year_totals = []
+year_labels = []
 
 # compounding func appending each year to total list
 def calc_investing(principal, rate, num_comp, time, contribution, counter):
     for year in range(time):
         principal += contribution
         amount = principal * (pow((1 + rate / num_comp), num_comp * (year + 1)))
-        year_total.append(amount)
+        year_totals.append(amount)
         print(f'Year {year + 1} - ${round(amount, 2)}')
         counter += 1
     return amount, counter
@@ -35,7 +35,7 @@ def calc_retirement(principal, rate, num_comp, withdraw, counter):
     while principal > 0:
         if principal > withdraw:
             principal -= withdraw
-            year_total.append(principal)
+            year_totals.append(principal)
             print(f'Year {counter + 1} - ${round(principal, 2)}')
             counter += 1
         else:
@@ -50,7 +50,7 @@ print(f'Investment years: {principal}')
 print(f'Retirement years: {retire_years}')
 print(len(f'X labels: {x_labels}'))
 print(len(f'Y labels: {y_labels}'))
-print(f'Total Accrued: {year_total}')
+print(f'Total Accrued: {year_totals}')
 
 percentage = '{:.2%}'.format(int_rate)
 round_dollars = round(principal, 2)
@@ -61,8 +61,8 @@ fig, ax = plt.subplots(figsize=(14, 9))
 fig.suptitle(f'Compound principal + accrued interest over {comp_years} years', fontsize='20', fontweight='bold')
 plt.title(f'Interest Rate: {percentage} -- Retirement year total: ${comma_dollars}', fontsize='12', fontweight='regular')
 
-colors = ['yellowgreen' if i < max(year_total) else 'olivedrab' for i in year_total]
-plt.bar(range(counter), year_total, tick_label=range(counter), width=.5, color=colors)
+colors = ['yellowgreen' if i < max(year_totals) else 'olivedrab' for i in year_totals]
+plt.bar(range(counter), year_totals, tick_label=range(counter), width=.5, color=colors)
 plt.ylabel('Total value (millions)', fontweight='bold')
 ax.xaxis.set_label_coords(.5, -.07)
 plt.xlabel('Years of compounding/withdrawing', fontweight='bold')
