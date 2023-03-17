5878c1b2493406e563ea5e12a21b3fba072e453c
mattfwilson <mattfwilson@gmail.com>
2023-03-17 01:41:39 -0400

    Differently colored bars after retirement begins!

--- invest_graph_withdraw.py
+++ invest_graph_withdraw.py
@@ -1,80 +1,76 @@
-##### TO-DOS #####
-# Add in birthdate to calculate time to required retirement withdrawal
+##### TO-DOS ################################################################################################
+
 # Add function that calculates interest only
 # Create stacked bar graphs showing individual principal vs interest growth
+# Make y ticks more consumable
 
-import matplotlib
-matplotlib.use('TkAgg', force=True)
 from matplotlib import pyplot as plt
-from matplotlib.ticker import MaxNLocator
 import datetime
 
-principal = 675000
+age = 36
+principal = 625000
+int_principal = principal
 int_rate = .075
 num_comp = 12
 comp_years = 13
-counter = 0
-contribution = 30000
-withdrawal = 70000
-age = 36
-x_labels = []
-y_labels = []
+contribution = 40000
+withdrawal = 75000
 year_totals = []
 year_labels = []
+counter = 0
 
-# compounding func appending each year to total list
-def calc_investing(principal, rate, num_comp, time, contribution, counter):
+##### Compounding + withdrawal + peak total functions #########################################################
+
+def calc_investing(principal, rate, num_comp, time, contribution, year_count):
     for year in range(time):
         principal += contribution
         amount = principal * (pow((1 + rate / num_comp), num_comp * (year + 1)))
         year_totals.append(amount)
         print(f'Year {year + 1} - ${round(amount, 2)}')
-        counter += 1
-    return amount, counter
+        year_count += 1
+    return amount, year_count
 
-def calc_retirement(principal, rate, num_comp, withdraw, counter):
+def calc_retirement(principal, rate, num_comp, withdraw, year_count):
     while principal > 0:
         if principal > withdraw:
             principal -= withdraw
             year_totals.append(principal)
-            print(f'Year {counter + 1} - ${round(principal, 2)}')
-            counter += 1
+            print(f'Year {year_count + 1} - ${round(principal, 2)}')
+            year_count += 1
         else:
             principal -= principal
-    return principal, counter
+    return principal, year_count
 
-# count increment for graph tick labels
+def calc_peak_total(years):
+    today = datetime.date.today()  
+    current_year = today.year
+
+    for i in year_totals:
+        year_labels.append(current_year)
+        current_year += 1  
+    for total in years:
+        if total == max(years):
+            peak_total = total
+            max_index = years.index(peak_total)
+    return max_index
+
+##### Call functions and format results #######################################################################
+
 principal, counter = calc_investing(principal, int_rate, num_comp, comp_years, contribution, counter)
 retire_years, counter = calc_retirement(principal, int_rate, num_comp, withdrawal, counter)
-
-print(f'Investment years: {principal}')
-print(f'Retirement years: {retire_years}')
-print(len(f'X labels: {x_labels}'))
-print(len(f'Y labels: {y_labels}'))
-print(f'Total Accrued: {year_totals}')
+max_year = calc_peak_total(year_totals)
 
 percentage = '{:.2%}'.format(int_rate)
 round_dollars = round(principal, 2)
 comma_dollars = '{:,}'.format(round_dollars)
 
-# calculate year labels based on age and find peak year total
-today = datetime.date.today()  
-current_year = today.year
-for i in year_totals:
-    year_labels.append(current_year)
-    current_year += 1  
+##### Graph data ##############################################################################################
 
-for total in year_totals:
-    if total == max(year_totals):
-        max_index = total
-        year_index = year_totals.index(max_index)
-
-# graph data with matplot
 fig, ax = plt.subplots(figsize=(14, 9))
-fig.suptitle(f'Compound growth over of ${year_totals[0]} over {comp_years} years', fontsize='20', fontweight='bold')
-plt.title(f'Interest: {percentage} -- Year {year_labels[year_index]} peak savings: ${comma_dollars} -- {age + len(year_totals)} years old when savings runs out in {year_labels[-1]}', fontsize='12', fontweight='regular')
+fig.suptitle(f'Compound growth over of ${int_principal} over {comp_years} years', fontsize='20', fontweight='bold')
+plt.title(f'Interest: {percentage} -- Year {year_labels[max_year]} peak savings: ${comma_dollars} -- {age + len(year_totals)} years old when savings runs out in {year_labels[-1]}', fontsize='12', fontweight='regular')
 
-colors = ['yellowgreen' if i < max(year_totals) else 'olivedrab' for i in year_totals]
+colors = ['indianred' if year_totals.index(x) > max_year else 'olivedrab' for x in year_totals]
 plt.bar(range(counter), year_totals, tick_label=year_labels, width=.5, color=colors)
 plt.xticks(rotation=90)
 plt.ylabel('Total value (millions)', fontweight='bold')
