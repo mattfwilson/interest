47854e435b4bffe83e0bef57f24f2a1e0de9b737
mattfwilson <mattfwilson@gmail.com>
2024-02-07 16:02:10 -0500

    Added user input

--- stonks.py
+++ stonks.py
@@ -1,15 +1,24 @@
 import matplotlib.pyplot as plt
 import yfinance as yf
+import random
 
-ticker_symbol = '^GSPC'
-stock_data = yf.download(ticker_symbol, start='1950-01-01', end='1985-01-01')
+graph_colors = ['forestgreen', 'red', 'royalblue', 'goldenrod', 'darkorange', 'darkviolet', 'olivedrab', 'dimgray']
+ticker = 'AAPL'
+start_date = '2010-01-01'
+end_date = '2024-01-01'
+rand_lst = [random.randint(1, 1000) for _ in range(1000)]
+
+ticker = str(input('What stock would you like to parse? '))
+start_date = str(input(f'When should we start parsing for {ticker} (yyyy-mm-dd)? '))
+end_date = str(input(f'When should parsing end for {ticker} (yyyy-mm-dd)? '))
+stock_data = yf.download(ticker, start=start_date, end=end_date)
 
 plt.figure(figsize=(22, 12))
-plt.plot(stock_data['Close'], label='Close', color='blue')
+plt.plot(stock_data['Close'], label=ticker, color=random.choice(graph_colors))
 
 plt.xlabel('Date')
-plt.ylabel('Price')
-plt.title('Stock Price of {}'.format(ticker_symbol))
+plt.ylabel('Stock Price')
+plt.title('Stock closing prices for {} from {} - {}'.format(ticker, start_date, end_date))
 plt.legend()
 
 plt.show()
