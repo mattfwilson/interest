178c5a2ac0445e0690bdbc8ad16b211c95dfcb9f
mattfwilson <mattfwilson@gmail.com>
2024-02-08 12:00:45 -0500

    Graph multiple stocks and randomize their line colors

--- stonks.py
+++ stonks.py
@@ -4,6 +4,7 @@ import yfinance as yf
 import random
 import pyautogui
 
+stonks = ['AAPL', 'AMZN', 'MSFT']
 graph_colors = ['forestgreen', 'red', 'royalblue', 'goldenrod', 'darkorange', 'darkviolet', 'olivedrab', 'dimgray']
 ticker = 'AAPL'
 start_date = '2010-01-01'
@@ -16,16 +17,16 @@ def full_screen():
 #ticker = str(input('What stock would you like to parse? '))
 #start_date = str(input(f'When should we start parsing for {ticker} (yyyy-mm-dd)? '))
 #end_date = str(input(f'When should parsing end for {ticker} (yyyy-mm-dd)? '))
-stock_data = yf.download(ticker, start=start_date, end=end_date)
+stock_data = yf.download(stonks, start=start_date, end=end_date)
 
 plt.figure(figsize=(16, 10))
-plt.plot(stock_data['Close'], label=ticker, color=random.choice(graph_colors))
+plt.plot(stock_data['Close'], label=stonks, color=random.choice(graph_colors))
 full_screen()
 
 #plt.gca().yaxis.set_major_formatter(ticker.FormatStrFormatter('%0.1f'))
 plt.xlabel('Date', fontweight='bold', fontsize=16)
 plt.ylabel('Stock Price', fontweight='bold', fontsize=16)
-plt.title('Closing prices for {} from {} - {}'.format(ticker, start_date, end_date), fontweight='bold', fontsize=24, y=1.04)
+plt.title('Closing prices for {} from {} - {}'.format(stonks, start_date, end_date), fontweight='bold', fontsize=24, y=1.04)
 plt.legend()
 
 plt.show()
---
+++ stonks2.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import yfinance as yf
+import random
+import pyautogui
+
+stock_lst = ['AAPL', 'AMZN', 'MSFT']
+colors = ['forestgreen', 'red', 'royalblue', 'goldenrod', 'darkorange', 'darkviolet', 'olivedrab', 'dimgray', 'lightblue', 'orange']
+ticker = 'AAPL'
+start_date = '2010-01-01'
+end_date = '2024-01-01'
+stock_data = yf.download(stock_lst, start=start_date, end=end_date)
+
+def full_screen():
+    pyautogui.hotkey('alt', 'space')
+    pyautogui.hotkey('x')
+
+def individualize(stocks):
+    for stock in stocks:
+        selected_color = random.choice(colors)
+        colors.remove(selected_color)
+        stock_data = yf.download(stock, start=start_date, end=end_date)
+        plt.plot(stock_data['Close'], label=stock, color=selected_color)
+
+
+individualize(stock_lst)
+
+plt.xlabel('Date', fontweight='bold', fontsize=16)
+plt.ylabel('Stock Price', fontweight='bold', fontsize=16)
+plt.title('Closing prices for {} from {} - {}'.format(stock_lst, start_date, end_date), fontweight='bold', fontsize=24, y=1.04)
+plt.legend()
+
+full_screen()
+plt.show()
+
