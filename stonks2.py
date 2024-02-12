07e2043b2671f975349bf7960224ff9f17c7ac48
mattfwilson <mattfwilson@gmail.com>
2024-02-12 00:45:14 -0500

    Re-organizing and removed unnecessary code

--- stonks2.py
+++ stonks2.py
@@ -5,7 +5,6 @@ import pyautogui
 
 stock_lst = ['AAPL', 'AMZN', 'MSFT', 'NVDA', 'TSLA']
 colors = ['red', 'royalblue', 'goldenrod', 'darkorange', 'darkviolet', 'olivedrab', 'dimgray']
-ticker = 'AAPL'
 start_date = '2010-01-01'
 end_date = '2024-01-01'
 stock_data = yf.download(stock_lst, start=start_date, end=end_date)
@@ -16,14 +15,15 @@ def full_screen():
 
 def clean_date(date):
     months = {'01': 'Jan', '02': 'Feb', '03': 'Mar', '04': 'Apr', '05': 'May', '06': 'Jun', '07': 'Jul', '08': 'Aug', '09': 'Sep', '10': 'Oct', '11': 'Nov', '12': 'Dec'}
+    clean_year = date[:4]
     clean_month = months[date[-5:-3]]
     clean_day = ''
     if date[-2] == '0':
         clean_day = date[-1]
     else:
         clean_day = date[-2:]
-    clean_year = date[:4]
     clean_date = clean_month + ' ' + clean_day + ', ' + clean_year
+
     return clean_date
 
 def parse_stocks(stocks):
