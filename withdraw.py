a2cc86e8628c7c3d989dfd8c6b0372333dc5810e
mattfwilson <mattfwilson@gmail.com>
2024-02-18 02:38:08 -0500

    Update to use your age as the x axis

--- withdraw.py
+++ withdraw.py
@@ -2,7 +2,8 @@ import matplotlib.pyplot as plt
 import yfinance as yf
 import pyautogui
 
-principal = 850000
+age = 37
+principal = 650000
 int_rate = .06
 withdraw_rate = 45000
 inflation = .02
@@ -12,15 +13,15 @@ num_comp = 1
 x_axis = []
 y_axis = []
 
-def calc_retire(principal, int_rate, withdraw_rate, retire_years, num_comp):
+def calc_retire(principal, int_rate, withdraw_rate, age, num_comp):
     old_principal = principal
     principal = principal * pow(((1 + int_rate / num_comp)), (num_comp * 1))
     interest = principal - old_principal
     principal = principal - withdraw_rate
-    retire_years += 1
-    x_axis.append(retire_years)
+    age += 1
+    x_axis.append(age)
     y_axis.append(principal)
-    return principal, retire_years, interest
+    return principal, age, interest
 
 def full_screen():
     pyautogui.hotkey('alt', 'space')
@@ -38,11 +39,11 @@ def show_graph():
 
 if __name__ == '__main__':
     while principal > 0:
-        res = calc_retire(principal, int_rate, withdraw_rate, retire_years, num_comp)
+        res = calc_retire(principal, int_rate, withdraw_rate, age, num_comp)
         print(f'Year {res[1]} - Principal: ${round(res[0], 2):,} (Interest: ${round(res[2], 2):,}/year - ${round(res[2] / 12, 2)}/month)')
         principal = res[0]
-        retire_years = res[1]
-        if retire_years > 62:
+        age = res[1]
+        if age > 100:
             break
 
 show_graph()
