d197665a7d7b07fdc293ff09af0228869656e35d
mattfwilson <mattfwilson@gmail.com>
2025-02-25 00:11:42 -0500

    Shorter names for simplicity

--- invest_graph6.py
+++ invest_graph6.py
@@ -1,36 +1,36 @@
 import matplotlib.pyplot as plt
 
 principal = 1_150_000
-low_return_rate = 0.06
-high_return_rate = 0.10
-years = 20
+low_rate = 0.06
+high_rate = 0.10
+years = 10
 
 def convert_dollars(amount):
 	return f"${amount:,.2f}"
 
 def compound(principal, low_rate, high_rate, years):
-	low_balance = principal
-	high_balance = principal
+	low_bal = principal
+	high_bal = principal
 	low_int_rate = low_rate
 	high_int_rate = high_rate
 	c_years = years
 	years_lst = list(range(1, years + 1))
-	c_balance_low = []
-	c_balance_high = []
+	c_bal_low = []
+	c_bal_high = []
 
 	while c_years > 0:
-		low_balance *= round((1 + low_int_rate), 2)
-		c_balance_low.append(low_balance)
-		high_balance *= round((1 + high_int_rate), 2)
-		c_balance_high.append(high_balance)
+		low_bal *= round((1 + low_int_rate), 2)
+		c_bal_low.append(low_bal)
+		high_bal *= round((1 + high_int_rate), 2)
+		c_bal_high.append(high_bal)
 		c_years -= 1
 
-	print(f"{low_int_rate:.1%} Rate Final Balance:", convert_dollars(c_balance_low[-1]))
-	print(f"{high_int_rate:.1%} Rate Final Balance:", convert_dollars(c_balance_high[-1]))
+	print(f"{low_int_rate:.1%} Rate Final Balance:", convert_dollars(c_bal_low[-1]))
+	print(f"{high_int_rate:.1%} Rate Final Balance:", convert_dollars(c_bal_high[-1]))
 
 	plt.figure(figsize=(16, 10))
-	plt.plot(years_lst, c_balance_low, label="Low Rate", color="red", linestyle="--", linewidth=2)
-	plt.plot(years_lst, c_balance_high, label="High Rate", color="blue", linestyle="--", linewidth=2)
+	plt.plot(years_lst, c_bal_low, label="Low Rate", color="red", linestyle="--", linewidth=2)
+	plt.plot(years_lst, c_bal_high, label="High Rate", color="blue", linestyle="--", linewidth=2)
 
 	plt.xlabel("Time (Years)")
 	plt.ylabel("Balance ($)")
@@ -39,5 +39,5 @@ def compound(principal, low_rate, high_rate, years):
 	plt.grid()
 	plt.show()
 
-compound(principal, low_return_rate, high_return_rate, years)
+compound(principal, low_rate, high_rate, years)
 
